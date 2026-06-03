e98693d374b158a8b633e09c2d12efcab90228b7
Md Mahedi Hasan <mahedi0803061@gmail.com>
2026-06-03 15:55:15 -0400

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_buy = prices[0]
+        max_profit = 0
+
+        for p in prices:
+            if p <= min_buy:
+                min_buy = p
+            else:
+                max_profit = max(max_profit, p - min_buy)
+
+        return max_profit
