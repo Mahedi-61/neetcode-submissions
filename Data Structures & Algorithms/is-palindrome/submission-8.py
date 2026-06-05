6cea04199307f4237010c288effbf4ef312252b7
Md Mahedi Hasan <mahedi0803061@gmail.com>
2026-06-04 21:01:51 -0400

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        l = 0
+        r = len(s)-1
+        while l <= r:
+            if not (ord('a') <= ord(s[l]) <= ord('z') or 
+                    ord('A') <= ord(s[l]) <= ord('Z') or 
+                    ord('0') <= ord(s[l]) <= ord('9')):
+                l += 1
+                continue
+
+            if not (ord('a') <= ord(s[r]) <= ord('z') or 
+                      ord('A') <= ord(s[r]) <= ord('Z') or 
+                    ord('0') <= ord(s[r]) <= ord('9')):
+                r -= 1
+                continue
+
+            
+            if s[l].lower() != s[r].lower():
+                return False
+
+            else:
+                l += 1
+                r -= 1
+
+        return True
